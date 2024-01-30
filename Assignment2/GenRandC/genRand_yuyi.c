0f2f755d7d50637845597aae886cda452efcaec8
Bill-Yuyi <a274683718@gmail.com>
2024-01-30 10:13:24 -0800

    modified GenRandC

--- Assignment2/GenRandC/genRand_yuyi.c
+++ Assignment2/GenRandC/genRand_yuyi.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <ctype.h>
 
-unsigned long lcg(unsigned long *seed) {
+unsigned long lcg(unsigned long *seed)
+{
     const unsigned long a = 22695477;
     const unsigned long c = 1;
     const unsigned long m = 2147483648;
@@ -13,99 +14,65 @@ unsigned long lcg(unsigned long *seed) {
     return *seed;
 }
 
-int genRand(int min, int max) {
-    static unsigned long seed = 0; 
+int genRand(int min, int max)
+{
+    static unsigned long seed = 0;
 
-    if (seed == 0) {
+    if (seed == 0)
+    {
         seed = time(NULL);
     }
 
     return (lcg(&seed) % (max - min + 1)) + min;
 }
 
-int isDigitString(const char *str) {
-    while (*str) {
-        if (!isdigit((unsigned char)*str)) {
-            return 0; 
+int isDigitString(const char *str)
+{
+    while (*str)
+    {
+        if (!isdigit((unsigned char)*str))
+        {
+            return 0;
         }
         str++;
     }
-    return 1; 
+    return 1;
 }
 
-int fileExists(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file != NULL) {
-        fclose(file);
-        return 1; 
-    }
-    return 0; 
-}
+int main(int argc, char *argv[])
+{
+    unsigned long seed = time(NULL);
 
-int main(int argc, char *argv[]) {
-    unsigned long seed = time(NULL); 
-
-    if(argc != 3 && argc != 4) {
-        printf("Please provide at least 2 arguments including a number and a file name or 3 arguments with writing mode\n");
+    if (argc != 3 && argc != 4)
+    {
+        printf("Usage: %s <number> <filename> [-a]\n", argv[0]);
         return -1;
     }
-   
-    if(!isDigitString(argv[1])){
+
+    if (!isDigitString(argv[1]))
+    {
         printf("Please give a valid number in the first argument");
         return -1;
     }
 
     int num = atoi(argv[1]);
-    char* filename = argv[2];
-    char* mode = (char*)malloc(2 * sizeof(char));
-    
-    if(argc == 4) {
-        if(strcmp(argv[3], "-a") == 0) {
-            mode = "-a";
-        }else {
-            printf("Please provide valid argument ('-a' or nothing)\n");
-            return -1;
-        }
-    }
-    
-    // if mode is -a
-    if(strcmp(mode,"-a") == 0) {
-        if(fileExists(filename) == 0) {
-            printf("File not exists\n");
-            return -1;
-        }
+    char *filename = argv[2];
+    char *mode = argc == 4 && strcmp(argv[3], "-a") == 0 ? "a" : "w";
 
-        FILE *file = fopen(filename, "a");
-        if(file == NULL) {
-            printf("Error opening file\n");
-            return -1;
-        }
-
-        for(int i = 0; i < num; i++) {          
-           fprintf(file, "%d ", genRand(1,100));
-        }
-        fprintf(file, "\n");
-        
-        fclose(file);
+    FILE *file = fopen(filename, mode);
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return -1;
     }
 
-    // mode is not -a
-    else {
-        FILE *file = fopen(filename, "w");
-        if(file == NULL) {
-            printf("Error opening file\n");
-            return -1;
-        }
-
-        for(int i = 0; i < num; i++) {          
-            fprintf(file, "%d ", genRand(1,100));
-        }
-        fprintf(file, "\n");
-        
-        fclose(file);
+    for (int i = 0; i < num; i++)
+    {
+        fprintf(file, "%d ", genRand(1, 100));
     }
 
-    free(mode);
+    fprintf(file, "\n");
+
+    fclose(file);
     return 0;
-
 }
