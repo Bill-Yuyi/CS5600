966336dad95b16641b239b427c7d126aba25b105
Bill-Yuyi <a274683718@gmail.com>
2024-04-05 14:57:38 -0700

    modified some function

--- Practicum2/handle_request.c
+++ Practicum2/handle_request.c
@@ -9,26 +9,31 @@
 
 void write_request(char* rest, char* remote_path,char server_message[]) {
     pthread_mutex_lock(&file_operation_mutex);
+
     long file_size = atol(strtok_r(rest, "|", &rest));
     char* file_content = rest;
+
     if(write_file(remote_path, file_content, file_size) == 0) {
         printf("Content written to %s successfully\n", remote_path);
+        strcpy(server_message, "Writing file success!");
     }else {
         printf("Failed to write content to %s\n", remote_path);
+        strcpy(server_message, "Failed to write content to %s\n", remote_path);
     }
-    strcpy(server_message, "Writing file success!");
+
     pthread_mutex_unlock(&file_operation_mutex);
 }
 
 void get_request(char* remote_path, long file_size,char server_message[]) {
     pthread_mutex_lock(&file_operation_mutex);
+
     char* content = read_file(remote_path, &file_size);
     if(content == NULL) {
-        perror("memory allocation for file content is failed");
-        exit(1);
+        strcpy(server_message, "No such file");
+    }else {
+        strcpy(server_message, content);
+        free(content);
     }
-    strcpy(server_message, content);
-    free(content);
     pthread_mutex_unlock(&file_operation_mutex);
 }
 
@@ -37,7 +42,7 @@ void remove_request(char* remote_path, char server_message[]) {
     if (remove(remote_path) == 0) {
         strcpy(server_message, "File deleted!");
     } else {
-        perror("Error deleting file");
+        strcpy(server_message, "Error deleting file!");
     }
     pthread_mutex_unlock(&file_operation_mutex);
 }
